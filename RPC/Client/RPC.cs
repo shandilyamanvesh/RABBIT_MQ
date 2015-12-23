47ccfcd69b0a2e5daae13ad75330b93957204854
shandilyamanvesh <shandilyamanvesh@gmail.com>
2015-12-23 22:08:11 +0530

    RPC using rabbitmq

---
+++ RPC/Client/RPC.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class RPC
+    {
+        public static void Main()
+        {
+            var rpcClient = new RPCClient();
+
+            Console.WriteLine(" [x] Requesting fib(30)");
+            var response = rpcClient.Call("30");
+            Console.WriteLine(" [.] Got '{0}'", response);
+
+            rpcClient.Close();
+        }
+    }
+}
